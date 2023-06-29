1caf3e3a8e66fc8f9cc6757cc03cd91ce4c484a4
Vitaly Kryanin <krianinvv@internet.ru>
2023-06-29 18:50:20 +0300

    add auth

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const router = require('./routes');
 const app = express();
 const { PORT = 3000 } = process.env;
 app.use(express.json());
-app.use(router);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '649da75c3d4621294cf03e36',
+  };
+
+  next();
+});
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log('Слушаю порт 3000');
