988fb64a97c177d404094137a6e5f76bebde8eec
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-05 17:53:02 +0300

    fix, add getInfo

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,12 @@ const {
   createUser,
   updateAvatar,
   updateProfile,
+  getInfo
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
+router.get('/me', getInfo);
 router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
