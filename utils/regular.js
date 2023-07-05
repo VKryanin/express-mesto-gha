c8273b22577121bf94dd08f90316ac5114d454a5
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-05 22:31:43 +0300

    fixed regexr

--- utils/regular.js
+++ utils/regular.js
@@ -1,3 +1,3 @@
-const urlCheking = /^(https?:\/\/)(www\.)?[a-z0-9-._~:/?#[\]@!$&amp;'()*+,;=]+#?$/i
+const urlCheking = /^(https?:\/\/)(www\.)?[a-z0-9-._~:/?#[\]@!$&amp;'()*+,;=]+(\.[a-z]*)+(\/?)+#?$/i
 
 module.exports = urlCheking;
