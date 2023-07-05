3ad09e85a722c32898d3d3984ad760bc874c4a5b
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-05 22:39:26 +0300

    hot fix reg[1]

--- utils/regular.js
+++ utils/regular.js
@@ -1,3 +1,3 @@
-const urlCheking = /^(https?:\/\/)(www\.)?[a-z0-9-._~:/?#[\]@!$&amp;'()*+,;=]+(\.[a-z]+[a-z]+)+(\/?)+#?$/i
+const urlCheking = /^(https?:\/\/)(www\.)?[a-z0-9-._~:/?#[\]@!$&amp;'()*+,;=]+(\.[a-z]{2,3})+(\/?)+#?$/i
 
 module.exports = urlCheking;
