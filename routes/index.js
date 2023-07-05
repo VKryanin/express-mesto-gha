517ee8d3ad14bf7aff5687a2dcf5c1ee32191c1f
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-05 17:39:15 +0300

    add regular

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { ERROR_NOT_FOUND } = require('../utils/status');
 const { celebrate, Joi } = require('celebrate');
+const urlCheking = require('../utils/regular')
+const auth = require('../midlwares/auth');
+const { createUser, login } = require('../controllers/users');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
@@ -14,7 +17,7 @@ router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(3).max(30),
-    avatar: Joi.string().pattern(urlPattern),
+    avatar: Joi.string().pattern(urlCheking),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
---
+++ utils/regular.js
@@ -0,0 +1,3 @@
+const urlCheking = /^(https?:\/\/)(www\.)?[a-z0-9-._~:/?#[\]@!$&amp;'()*+,;=]+#?$/i
+
+module.exports = urlCheking;
