8fcbfa7bea3124f5446ba3a3fcfdb3824027817f
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-06 09:47:04 +0300

    fix routes

--- routes/users.js
+++ routes/users.js
@@ -11,24 +11,19 @@ const {
 const { urlCheking } = require('../utils/regular');
 
 router.get('/', getUsers);
-
+router.get('/me', getInfo);
 router.get('/:id', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().required().hex().length(24),
   }),
 }), getUserById);
-
-router.get('/me', getInfo);
-
 router.post('/', createUser);
-
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(3).max(30),
   }),
 }), updateProfile);
-
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(urlCheking),
