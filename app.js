3d5e66c06592aee11f9470134923dc59b3efa479
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-06 12:13:55 +0300

    teset

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(DB_URL, {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
--- routes/users.js
+++ routes/users.js
@@ -8,26 +8,31 @@ const {
   updateProfile,
   getInfo,
 } = require('../controllers/users');
-const { urlCheking } = require('../utils/regular');
+const urlCheking = require('../utils/regular');
 
 router.get('/', getUsers);
+
 router.get('/me', getInfo);
+
 router.get('/:id', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().required().hex().length(24),
   }),
 }), getUserById);
+
 router.post('/', createUser);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(3).max(30),
   }),
 }), updateProfile);
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(urlCheking),
-  }),
-}), updateAvatar);
+
+// router.patch('/me/avatar', celebrate({
+//   body: Joi.object().keys({
+//     avatar: Joi.string().required().pattern(urlCheking),
+//   }),
+// }), updateAvatar);
 
 module.exports = router;
