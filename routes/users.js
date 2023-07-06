f24b2881137162bfe000c28a1654ee6c0d97077f
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-07-06 09:18:41 +0300

    add valid Joi

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,31 @@ const {
   updateProfile,
   getInfo
 } = require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
 
 router.get('/', getUsers);
-router.get('/:id', getUserById);
+
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().required().hex().length(24),
+  }),
+}), getUserById);
+
 router.get('/me', getInfo);
+
 router.post('/', createUser);
-router.patch('/me', updateProfile);
-router.patch('/me/avatar', updateAvatar);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(3).max(30),
+  }),
+}), updateProfile);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(urlPattern),
+  }),
+}), updateAvatar);
 
 module.exports = router;
